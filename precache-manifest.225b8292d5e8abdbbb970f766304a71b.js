7c1f63c5f5546d4bbdd3a14abdedaa8dcca94608
fingerpich <zarei.bs@gmail.com>
2019-02-27 21:10:58 +0330

    Updates

---
+++ precache-manifest.225b8292d5e8abdbbb970f766304a71b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ca7362cc02e8c78ee6f3",
+    "url": "/static/js/main.ca7362cc.chunk.js"
+  },
+  {
+    "revision": "410e44e0ad8ac22613cf",
+    "url": "/static/js/2.410e44e0.chunk.js"
+  },
+  {
+    "revision": "ca7362cc02e8c78ee6f3",
+    "url": "/static/css/main.3f8bc2c3.chunk.css"
+  },
+  {
+    "revision": "f8e284c7286b39b6783c6aa4a420069d",
+    "url": "/index.html"
+  }
+];
